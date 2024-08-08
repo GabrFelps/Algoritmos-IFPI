59665d766c84e8e2b11c79242f40b0bec42a14c3
Felipe Gabriel <felipemrqs07@gmail.com>
2024-08-08 09:16:39 -0300

    add: modularização 1024 beecrowd

--- Bee-Crowd/strings/1042.py
+++ Bee-Crowd/strings/1042.py
@@ -4,43 +4,68 @@ def main():
     frases_resultado = []
 
     for numero in range(N):
-        frase_volta_1 = ""
-        frase_volta_2 = ""
-        frase_volta_3 = ""
         frase = input()
 
-        # caracteres da frase que tem letras minúsculas e maiúsculas 3 posições deslocadas para a direita
-        for letra in frase:
-            if (ord(letra) >= 65 and ord(letra) <= 90) or (ord(letra) >= 97 and ord(letra) <= 122):
-                ascii = ord(letra) + 3
-                letra = chr(ascii)   
-            else:
-                letra = letra
-            frase_volta_1 += letra
-
-
-        # frase invertida
-        letra_anterior = ""
-        for letra in frase_volta_1:
-            letra_anterior = letra + letra_anterior
-
-        frase_volta_2 = letra_anterior
-
-        # frase truncada
-        metade_qtd_caracteres_frase = len(frase_volta_2) // 2
-
-        for n_letra in range(len(frase_volta_2)):
-            if n_letra >= metade_qtd_caracteres_frase:
-                letra = frase_volta_2[n_letra]
-                ascii = ord(letra) - 1
-                letra = chr(ascii)
-            else:
-                letra = frase_volta_2[n_letra]
-            frase_volta_3 += letra
-
+        frase_volta_1 = realizar_primeira_volta(frase)
+        frase_volta_2 = realizar_segunda_volta(frase_volta_1)
+        frase_volta_3 = realizar_terceira_volta(frase_volta_2)    
+        
+        # armazena a frase totalmente criptografada em uma lista
         frases_resultado.append(frase_volta_3)
 
+    # percorre os valores da lista dos resultados e as mostra    
     for frases in frases_resultado:
         print(frases)
+
+
+
+def realizar_primeira_volta(frase):
+# caracteres da frase que são letras (maiúsculas e minúsculas) 3 posições deslocadas para a direita
+    frase_volta_1 = ""
+    for letra in frase:
+        if eh_letra(letra):
+            codigo_ascii = ord(letra) + 3
+            letra = chr(codigo_ascii)
+            
+        frase_volta_1 += letra
+
+    return frase_volta_1
+
+
+def realizar_segunda_volta(frase_volta_1):
+    # inverter a frase
+    frase_volta_2 = ""
+    letra_anterior = ""
+    for letra in frase_volta_1:
+        letra_anterior = letra + letra_anterior
+
+    frase_volta_2 = letra_anterior
+    
+    return frase_volta_2
+
+
+def realizar_terceira_volta(frase_volta_2):
+    # truncar a frase e alterar a metade superior dela 
+    frase_volta_3 = ""
+    metade_qtd_caracteres_frase = retornar_metade_frase(len(frase_volta_2))
+
+    for n_letra in range(len(frase_volta_2)):
+        if n_letra >= metade_qtd_caracteres_frase:
+            letra = frase_volta_2[n_letra]
+            codigo_ascii = ord(letra) - 1
+            letra = chr(codigo_ascii)
+        else:
+            letra = frase_volta_2[n_letra]
+
+        frase_volta_3 += letra
+
+    return frase_volta_3
+
+def eh_letra(letra):
+    # retorna se os caracteres da frase 
+    return (ord(letra) >= 65 and ord(letra) <= 90) or (ord(letra) >= 97 and ord(letra) <= 122)
+
+def retornar_metade_frase(tamanho_frase):
+    return tamanho_frase // 2
 
 main()
